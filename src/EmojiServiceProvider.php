9f0edc147c70027c33e59ecd03aafe59e5dea155
Graham Campbell <graham@alt-three.com>
2017-12-31 17:59:14 +0000

    Update EmojiServiceProvider.php

--- src/EmojiServiceProvider.php
+++ src/EmojiServiceProvider.php
@@ -43,7 +43,7 @@ class EmojiServiceProvider extends ServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__.'/../config/emoji.php');
+        $source = realpath($raw = __DIR__.'/../config/emoji.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('emoji.php')]);
